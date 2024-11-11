61e16aeb7b28006f3a71bafc5457b16f11b6e342
Abhay Bhandarkar <105074579+AbhayBhandarkar@users.noreply.github.com>
2024-11-11 19:57:33 +0530

    Update vsGenEV.py

--- vsGenEV.py
+++ vsGenEV.py
@@ -3,7 +3,7 @@ import json
 from sentence_transformers import SentenceTransformer
 
 # MongoDB connection
-conn = pymongo.MongoClient("mongodb+srv://abhaybhandarkar2:<db_password>@cluster0.jzzsl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+conn = pymongo.MongoClient("yourmongodbcluster")
 db = conn['education']
 coll = db['courses']
 
@@ -11,7 +11,7 @@ coll = db['courses']
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Path to the JSON file
-file_path = "/Users/mangalabhandarkar/Downloads/Archive (3)/medical_engineering_courses.json"
+file_path = "yourfilename.json"
 
 # Open JSON file and read line by line
 with open(file_path, "r") as file:
